2f6e0652f4a608a34b094f0adf47370742632ce7
pranshu200 <pranshuy2020@gmail.com>
2021-07-13 04:21:15 +0530

    Problem 171. Excel Sheet Column Number

---
+++ Problem171.cpp
@@ -0,0 +1,13 @@
+/* 171. Excel Sheet Column Number */
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int res=0;
+        for(int i=0;i<columnTitle.length();i++)
+        {
+            res=res*26 + (columnTitle[i]-'A'+1);
+        }
+        return res;
+    }
+};
