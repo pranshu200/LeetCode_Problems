52980a91d86a65fe0bd947c1512907e3310bea54
pranshu200 <pranshuy2020@gmail.com>
2021-07-18 16:59:03 +0530

    Problem561. Array Partition I

---
+++ Problem561.cpp
@@ -0,0 +1,14 @@
+/* 561. Array Partition I */
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        long long int res=0;
+        for(int i=0;i<nums.size();i+=2)
+        {
+            res+=(nums[i]);
+        }
+        return res;
+    }
+};
