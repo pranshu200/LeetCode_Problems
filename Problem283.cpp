54dc82930618db99d4f775338b61049b74ebadf1
pranshu200 <pranshuy2020@gmail.com>
2021-07-29 18:16:20 +0530

    Problem 283. Move Zeroes

---
+++ Problem283.cpp
@@ -0,0 +1,16 @@
+/* 283. Move Zeroes */
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i] != 0)
+            {
+                swap(nums[j],nums[i]);
+                j++;
+            }
+        }     
+    }
+};
